2dddc451f76c34e37e74ed9423761ecfe6532e5f
Prakhar <gprakhar11@gmail.com>
2020-05-03 14:53:56 +0530

    models

---
+++ backend/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.User)
+admin.site.register(models.Project)
+admin.site.register(models.Issue)
+admin.site.register(models.Comments)
---
+++ backend/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from djrichtextfield.models import RichTextField
+from taggit.managers import TaggableManager
+from .managers import CustomUserManager
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here. 
+
+class User(AbstractUser):
+    profile = models.FilePathField()
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, related_name='project_user', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    wiki = RichTextField()
+    date = models.DateTimeField(auto_now_add=True)
+    version = models.DecimalField(max_digits=4, decimal_places=2)
+    members = models.ManyToManyField(User, related_name='member_user')
+
+
+class Issue(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    reported_user = models.ForeignKey(User, related_name='reported_user', on_delete=models.SET_NULL, null=True)
+    assigned_user = models.ForeignKey(User, related_name='assigned_user', on_delete=models.PROTECT)
+    heading = models.CharField(max_length=100)
+    description = RichTextField(blank=True, null=True)
+    tags = TaggableManager()
+    media = models.FileField(upload_to=f'issue_media/{project.name}', blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, related_name='comment_user', on_delete=models.SET_NULL, null=True)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_user')
